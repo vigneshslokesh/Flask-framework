e9bf1b8e4b83ad2a62a9484494803eb0e82f4b0e
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-03 05:10:25 +0530

    Flask program

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-app =Flask(__name__)
 
-@app.route('/')
+# WSGI app is created 
+app =Flask(__name__) #WSGI standard interacting with the server
+
+@app.route('/') #decorator with the binding with function
 def welcome():
     return 'Welcome to my Flask framework'
 
