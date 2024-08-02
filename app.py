a645cd2ded5c1e470703024013f3fcc5d8bee200
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-03 04:21:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# from flask import Flask
+# app =Flask(__name__)
+
+# @app.route('/')
+# def welcome():
+#     return 'Welcome to my Flask framework'
+
+# if __name__ == "__main__":
+#     app.run()
+
+
+msg = "Welcome to my first program"
+print(msg)
+print("Please like and subscribe")
