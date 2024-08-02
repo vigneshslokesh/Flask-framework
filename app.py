90b5cf2e2ab4dbcdc8c9e39b9274f187d55024df
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-03 05:15:44 +0530

    flask program

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask
 # WSGI app is created 
 app =Flask(__name__) #WSGI standard interacting with the server
 
-@app.route('/') #decorator with the binding with function
-def welcome():
+@app.route('/') #decorator with the binding with function arg-(URL) (/ - root url)
+def welcome(): #binding function
     return 'Welcome to my Flask framework'
 
+@app.route('/members') 
+def members():
+    return 'Welcome to my Flask framework members'
+
 if __name__ == "__main__":
-    app.run()
+    app.run()   
