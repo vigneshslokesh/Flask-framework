fd81a098131a59bdd42be524272f9d99d72b20cf
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-03 04:25:27 +0530

    flask framework

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-# from flask import Flask
-# app =Flask(__name__)
+from flask import Flask
+app =Flask(__name__)
 
-# @app.route('/')
-# def welcome():
-#     return 'Welcome to my Flask framework'
+@app.route('/')
+def welcome():
+    return 'Welcome to my Flask framework'
 
-# if __name__ == "__main__":
-#     app.run()
-
-
-msg = "Welcome to my first program"
-print(msg)
-print("Please like and subscribe")
+if __name__ == "__main__":
+    app.run()
